36fc8d6fd89655417bf7ade8cff6cf1b92ca8518
Josh Snyder <joshs@netflix.com>
2017-12-14 12:48:10 -0800

    Add two new scripts for debugging delay accounting.
    delayacct.py is designed to be used with current (e.g. 4.13.0) kernels.
    delayacct_new.py is meant for a patched version of the kernel.
    
    They each print the pointer of the delays struct that is updated when block I/O
    ends. As an example:
    
     <idle>-0     [005] d.h. 68976.200976: 0x00000001: Updating stats at: ffff8881bb40e280
    
    On nodes that have the bug, the running process (first column) directly
    determines which delays struct is updated. On nodes that where the problem is
    fixed, the running process has no effect on the updated pointer.

---
+++ examples/tracing/delayacct_new.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+from bcc import BPF
+
+REQ_WRITE = 1		# from include/linux/blk_types.h
+
+# load BPF program
+b = BPF(text="""
+#include <uapi/linux/ptrace.h>
+#include <linux/spinlock_types.h>
+#include <linux/delayacct.h>
+#include <linux/sched.h>
+
+#define container_of(ptr, type, member) ({			\
+	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+	(type *)( (char *)__mptr - offsetof(type,member) );})
+
+void trace_completion(struct pt_regs *ctx, spinlock_t* lock, u64* start, u64* total, u32* count) {
+	struct task_delay_info * delays = container_of(lock, struct task_delay_info, lock);
+	bpf_trace_printk("Updating stats at: %p\\n", delays);
+}
+""")
+
+b.attach_kprobe(event="delayacct_end", fn_name="trace_completion")
+
+# format output
+while True:
+	b.trace_print()
